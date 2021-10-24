4fdde47512ec3f66d3882ca8313417f2b78fd5e5
cramik <5573629+cramik@users.noreply.github.com>
2021-10-24 14:43:51 -0700

    Update check.py
    oops

--- check.py
+++ check.py
@@ -6,4 +6,4 @@ channels = [
 ] #INSERT CHANNEL IDs, typically UC... (seperated by commas and enclosed in quotes because python)
 
 for channel in channels:
-        if not (api.get_channel_info(channel_id=channel).items): print("Channel " + channel + " broke") #This will only return true if the API returns an empty with the channel
+        if not (api.get_channel_info(channel_id=channel).items): print("Channel " + channel + " broke") #This will only return true if the API returns an empty list with the channel
